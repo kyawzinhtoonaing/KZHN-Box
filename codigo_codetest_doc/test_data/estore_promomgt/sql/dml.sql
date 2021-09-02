33fe12d3d5c8e253d15725e7fab59a6a0550cfae
kyawzinhtoonaing <kyawzinhtoonaing90@gmail.com>
2021-09-03 04:43:49 +0630

    test data added

---
+++ codigo_codetest_doc/test_data/estore_promomgt/sql/dml.sql
@@ -0,0 +1,5 @@
+insert into tbl_user_payment_method
+values ('3571-0950-0102-1234', 'kzhn', '003', 100000);
+
+insert into tbl_user_payment_method
+values ('4687-8520-0102-1234', 'kzhn', '001', 50000);
