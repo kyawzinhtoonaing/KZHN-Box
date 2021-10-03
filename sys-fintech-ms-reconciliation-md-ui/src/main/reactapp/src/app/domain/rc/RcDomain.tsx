be35bed38ab9b4f0dc42d0e9f2dade2f4c87bf2f
kyawzinhtoonaing <kyawzinhtoonaing90@gmail.com>
2021-10-03 16:52:56 +0630

    pom change

--- sys-fintech-ms-reconciliation-md-ui/src/main/reactapp/src/app/domain/rc/RcDomain.tsx
+++
@@ -1,7 +0,0 @@
-import { CommonTemplate } from '../template/CommonTemplate';
-
-export function RcDomain() {
-    return (  
-        <CommonTemplate />
-    );
-}
--- sys-fintech-ms-reconciliation-md-ui/src/main/reactapp/src/app/util/ComponentEventUtil.ts
+++
@@ -1,4 +0,0 @@
-export interface ComponentEvent<PayloadType> {
-    eventId: string;
-    payload: PayloadType
-}
